28e651d33180fa5101ffc9dded4de0b69e219ecd
Mohammad <mohammadnabavi1384@gmail.com>
2026-07-26 15:23:34 +0330

    content model

--- conf/settings.py
+++ conf/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts.apps.AccountsConfig',
+    'content.apps.ContentConfig',
 ]
 
 MIDDLEWARE = [
---
+++ content/models.py
@@ -0,0 +1,29 @@
+import uuid
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Contact(models.Model):
+    content_uuid = models.UUIDField(primary_key=True ,default=uuid.uuid4, editable=False)
+    persian_name = models.CharField(max_length=100)
+    english_name = models.CharField(max_length=100)
+    summary = models.TextField()
+    is_special_show = models.BooleanField(default=False)
+    release_year = models.IntegerField()
+    director = models.CharField(max_length=100)
+    imdb_rate = models.FloatField()
+    duration = models.IntegerField()
+    age_rate = models.IntegerField()
+    is_free = models.BooleanField(default=False)
+    is_special_list = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    satisfaction_avg = models.FloatField(default=0)
+    is_dubbed = models.BooleanField(default=False)
+    like_count = models.IntegerField(default=0)
+    dislike_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.english_name

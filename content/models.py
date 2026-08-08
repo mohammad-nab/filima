4fd4530b26b0eacb00d6abcdb80ad02279ee0184
Mohammad <mohammadnabavi1384@gmail.com>
2026-08-09 00:46:19 +0330

    content mediator models

--- content/models.py
+++ content/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
+from tags.models import Genre, Country, Actor
 
 
 class Content(models.Model):
@@ -28,3 +29,34 @@ class Content(models.Model):
 
     def __str__(self):
         return self.english_name
+
+
+class ContentGenre(models.Model):
+    content_genre_uuid = models.UUIDField(primary_key=True ,default=uuid.uuid4, editable=False)
+    content_uuid = models.ForeignKey(Content, on_delete=models.CASCADE, related_name='content_genre')
+    genre_uuid = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='content_genre')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='created_content_genre')
+    updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='updated_content_genre')
+
+
+class ProducerCountry(models.Model):
+    producer_country_uuid = models.UUIDField(primary_key=True ,default=uuid.uuid4, editable=False)
+    content_uuid = models.ForeignKey(Content, on_delete=models.CASCADE, related_name='content_country')
+    country_uuid = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='content_country')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='created_producer_country')
+    updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='updated_product_country')
+
+
+class ContentActor(models.Model):
+    content_actor_uuid = models.UUIDField(primary_key=True ,default=uuid.uuid4, editable=False)
+    content_uuid = models.ForeignKey(Content, on_delete=models.CASCADE, related_name='content_actor')
+    actor_uuid = models.ForeignKey(Actor, on_delete=models.CASCADE, related_name='content_actor')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='created_actor')
+    updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='updated_actor')
+
--- content/views.py
+++ content/views.py
@@ -2,12 +2,22 @@ from rest_framework import viewsets
 from .models import Content
 from .serializers import ContentSerializer
 from rest_framework.permissions import IsAdminUser
+from conf.pagination import CustomPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
     permission_classes = [IsAdminUser]
+    pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # filterset_fields = []
+    search_fields = ['english_name', 'persian_name', 'content_uuid']
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
+    lookup_field = "slug"
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user,updated_by=self.request.user)

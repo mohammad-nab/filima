446ed34db85db83d56f50028c7c5e8644415da0c
Mohammad <mohammadnabavi1384@gmail.com>
2026-09-07 10:15:20 +0330

    dockerizing project

--- accounts/managers.py
+++ accounts/managers.py
@@ -15,7 +15,7 @@ class CustomerManager(BaseUserManager):
         if not kwargs['username']:
             raise ValueError("username must be set")
 
-        user = self.model(phone_number=kwargs['phone_number'], email=self.normalize_email(kwargs['email']) if kwargs['email'] else None,
+        user = self.model(phone_number=kwargs['phone_number'], email=self.normalize_email(kwargs.get('email')) if kwargs.get('email') else None,
                           first_name=kwargs['first_name'], last_name=kwargs['last_name'], username=kwargs['username'])
         user.set_password(kwargs['password'])
         user.save(using=self._db)
@@ -23,7 +23,7 @@ class CustomerManager(BaseUserManager):
 
     def create_superuser(self, *args, **kwargs):
         user = self.create_user(phone_number=kwargs['phone_number'], first_name=kwargs['first_name'], last_name=kwargs['last_name'],
-                            username=kwargs['username'], password=kwargs['password'], email=kwargs['email'])
+                            username=kwargs['username'], password=kwargs['password'], email=kwargs.get('email'))
         user.is_admin = True
         user.save(using=self._db)
         return user
